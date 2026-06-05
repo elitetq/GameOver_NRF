8bbf2bf5933bf1a031986fa4ab1e4ab2921cc9b6
EliteTQ <jonartbaj@gmail.com>
2026-06-05 17:41:30 -0600

    Initial commit: Zephyr app + J-Graphics-Library submodule

---
+++ app/src/main.c
@@ -0,0 +1,43 @@
+#include <zephyr/kernel.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/random/random.h>
+#include <zephyr/drivers/i2c.h>
+
+
+#include <BTN.h>
+#include <LED.h>
+
+#include <math.h>
+#include <J_GL.h>
+#include <J_ASSETS.h>
+
+// LOW_HEIGHT - MAX_HEIGHT - LOW_LENGTH - MAX_LENGTH
+uint16_t bounds[4] = {0x0000,LCD_MAX_HEIGHT,0x0000,LCD_MAX_LENGTH};
+
+const struct gpio_dt_spec dcx_gpio = GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE,dcx_gpios);
+const struct spi_cs_control cs_ctrl = (struct spi_cs_control) {
+  .gpio = GPIO_DT_SPEC_GET(ARDUINO_SPI_NODE,cs_gpios),
+  .delay = 0u,
+};
+
+const struct device * dev = DEVICE_DT_GET(ARDUINO_SPI_NODE);
+#define ARDUINO_I2C_NODE DT_NODELABEL(arduino_i2c)
+const struct device * dev_i2c = DEVICE_DT_GET(ARDUINO_I2C_NODE);
+const struct spi_config spi_cfg = {
+  .frequency = 15000000,
+  .operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB,
+  .slave = 0,
+  .cs = cs_ctrl
+};
+
+int main(void)
+{
+    J_init(dev,dev_i2c,&spi_cfg,&dcx_gpio,bounds);
+    J_LCD_init();
+    
+    return 0;
+}
